dbdea9c317995a75dd0c7f371ba4680955b08d01
jamiecohen <jamie.alex.cohen@gmail.com>
2024-01-30 17:03:38 -0800

    add another hiv incidence file

--- run_sim.py
+++ run_sim.py
@@ -138,7 +138,7 @@ def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_p
     dflocation = location.replace(' ', '_')
     # Make arguments
     if location == 'south africa':
-        hiv_datafile = ['data/hiv_incidence_south_africa_sens_2.csv',
+        hiv_datafile = ['data/hiv_incidence_south_africa_sens_3.csv',
                         'data/south_africa_female_hiv_mortality.csv',
                         'data/south_africa_male_hiv_mortality.csv']
         # art_datafile = ['data/south_africa_art_coverage_by_age_males.csv',
