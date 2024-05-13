6fbd11510248a985ca6da2639a81ec41dc341d4a
Robyn Stuart <robyna.s@gmail.com>
2024-05-13 14:04:53 +1000

    more deletions

--- run_sim.py
+++ run_sim.py
@@ -41,7 +41,7 @@ save_plots = True
 # %% Simulation creation functions
 def make_sim(
     location='south africa', calib=False, debug=0, datafile=None, hiv_datafile=None, calib_pars=None, n_agents=10e3,
-    art_datafile=None, vx_intv=None, hiv_death_adj=1, econ_analyzer=False, analyzer=None, end=None, seed=1,
+    art_datafile=None, vx_intv=None, hiv_death_adj=1, analyzers=None, end=None, seed=1,
     art_sens=False):
 
     if end is None:
@@ -64,8 +64,8 @@ def make_sim(
     )
 
     # Set initial conditions
-    pars.init_hpv_dist=dict(hpv16=0.4, hpv18=0.15, hi5=0.15, ohr=0.3)
-    pars.init_hpv_prev  = dict(
+    pars.init_hpv_dist = dict(hpv16=0.4, hpv18=0.15, hi5=0.15, ohr=0.3)
+    pars.init_hpv_prev = dict(
         age_brackets=np.array([  12,   17,   24,   34,  44,   64,    80, 150]),
         m=np.array([ 0.0, 0.25, 0.6, 0.25, 0.05, 0.01, 0.0005, 0]),
         f=np.array([ 0.0, 0.35, 0.7, 0.25, 0.05, 0.01, 0.0005, 0]),
@@ -84,8 +84,8 @@ def make_sim(
         f=dict(dist='normal', par1=17.7, par2=2.),  # DHS 2013
         m=dict(dist='normal', par1=18.2, par2=2.)   # No data for males, assumption
     )
-    pars.condoms = dict(m=0.01, c=0.1),
-    pars.eff_condoms = 0.5,
+    pars.condoms = dict(m=0.01, c=0.1)
+    pars.eff_condoms = 0.5
     pars.layer_probs = dict(    # Participation in marital and casual relationships
         m=np.array([
             [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75],
@@ -110,37 +110,26 @@ def make_sim(
         pars['hiv_pars']['cd4_ub'] = [5e6, 5e6]     # Upper bound for CD4 states
 
     # Analyzers
-    analyzers = sc.autolist()
     interventions = sc.autolist()
-    interventions += adjust_hiv_death(years=[1985,2000,2010], hiv_mort_adj=[1, 1, 1.5*hiv_death_adj])
+    interventions += adjust_hiv_death(years=[1985, 2000, 2010], hiv_mort_adj=[1, 1, 1.5*hiv_death_adj])
 
-    if not calib:
-        if len(vx_intv):
-            interventions += sp.get_vx_intvs(**vx_intv)
-
-        interventions += sp.get_screen_intvs(primary='hpv', screen_coverage=0.15, start_year=2020)
-
-        if econ_analyzer:
-            analyzers += an.econ_analyzer()
-
-        if analyzer is not None:
-            analyzers += analyzer
+    # if not calib:
+    #     if len(vx_intv):
+    #         interventions += sp.get_vx_intvs(**vx_intv)
+    #
+    #     interventions += sp.get_screen_intvs(primary='hpv', screen_coverage=0.15, start_year=2020)
 
     sim = hpv.Sim(pars=pars, analyzers=analyzers, interventions=interventions,
                   datafile=datafile, hiv_datafile=hiv_datafile, art_datafile=art_datafile, rand_seed=seed)
 
-
     return sim
 
 
+# %% Simulation running functions
 
-
-#%% Simulation running functions
-
-def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_pars=None, econ_analyzer=False,
-            debug=0, seed=0, label=None, meta=None, verbose=0.1, end=None, hivinc_datafile=None,
-            do_save=False, die=False, art_sens=False):
-    ''' Assemble the parts into a complete sim and run it '''
+def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_pars=None,
+            debug=0, seed=0, meta=None, verbose=0.1, end=None, hiv_datafile=None, art_datafile=None,
+            do_save=False, art_sens=False):
 
     # Decide what message to print
     if meta is not None:
@@ -151,19 +140,14 @@ def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_p
     print(msg)
     dflocation = location.replace(' ', '_')
 
-    if hivinc_datafile is None:
-        hivinc_datafile = 'hiv_incidence_south_africa'
     # Make arguments
-    if location == 'south africa':
-        hiv_datafile = [f'data/{hivinc_datafile}.csv',
-                        'data/south_africa_female_hiv_mortality.csv',
-                        'data/south_africa_male_hiv_mortality.csv']
-        # art_datafile = ['data/south_africa_art_coverage_by_age_males.csv',
-        #                 'data/south_africa_art_coverage_by_age_females.csv']
-        art_datafile = ['data/art_coverage_south_africa.csv']
-    else:
-        hiv_datafile = None
-        art_datafile = None
+    if hiv_datafile is None:
+        hiv_datafile = [f'data/hiv_incidence_{dflocation}.csv',
+                        f'data/{dflocation}_female_hiv_mortality.csv',
+                        f'data/{dflocation}_male_hiv_mortality.csv']
+    if art_datafile is None:
+        art_datafile = [f'data/art_coverage_{dflocation}.csv']
+
     # Make sim
     sim = make_sim(
         location=location,
@@ -172,7 +156,6 @@ def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_p
         n_agents=n_agents,
         hiv_death_adj=hiv_death_adj,
         calib_pars=calib_pars,
-        econ_analyzer=econ_analyzer,
         end=end,
         hiv_datafile=hiv_datafile,
         art_datafile=art_datafile,
@@ -184,10 +167,10 @@ def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_p
     # Store metadata
     sim.meta = sc.objdict()
     if meta is not None:
-        sim.meta = meta # Copy over meta info
+        sim.meta = meta  # Copy over meta info
     else:
         sim.meta = sc.objdict()
-    sim.meta.location = location # Store location in an easy-to-access place
+    sim.meta.location = location  # Store location in an easy-to-access place
 
     # Run
     sim['verbose'] = verbose
@@ -195,119 +178,111 @@ def run_sim(location=None, vx_intv=None, n_agents=50e3, hiv_death_adj=1, calib_p
     sim.shrink()
 
     if do_save:
-        sim.save(f'{ut.resfolder}/{dflocation}.sim')
+        sim.save(f'results/{dflocation}.sim')
 
     return sim
 
 
-def run_sims(locations=None, *args, **kwargs):
-    ''' Run multiple simulations in parallel '''
-
-    kwargs = sc.mergedicts(dict(debug=debug), kwargs)
-    simlist = sc.parallelize(run_sim, iterkwargs=dict(location=locations), kwargs=kwargs, serial=debug, die=True)
-    sims = sc.objdict({location:sim for location,sim in zip(locations, simlist)}) # Convert from a list to a dict
-
-    return sims
-
-
-#%% Run as a script
+# %% Run as a script
 if __name__ == '__main__':
 
     T = sc.timer()
-    location='south africa'
-    calib_filestem='_jan28'
+
+    # Load data and calibration parameters
+    location = 'south africa'
+    calib_filestem = '_jan28'
     dflocation = location.replace(' ', '_')
     calib_pars = sc.loadobj(f'results/{dflocation}_pars{calib_filestem}.obj')
-    analyzer=an.prop_exposed(years=[2020])
-    hiv_datafile = ['data/hiv_incidence_south_africa_sens.csv',
-                    'data/south_africa_female_hiv_mortality.csv',
-                    'data/south_africa_male_hiv_mortality.csv']
-    # art_datafile = ['data/south_africa_art_coverage_by_age_males.csv',
-    #                 'data/south_africa_art_coverage_by_age_females.csv']
-    art_datafile = ['data/art_coverage_south_africa.csv']
+    hiv_datafile = [f'data/hiv_incidence_south_africa_sens.csv',
+                    f'data/south_africa_female_hiv_mortality.csv',
+                    f'data/south_africa_male_hiv_mortality.csv']
+    art_datafile = [f'data/art_coverage_south_africa.csv']
+
+    # Add analyzers and interventions
+    analyzers = [an.prop_exposed(years=[2020])]
+
     # Make sim
-
-
-    sim = make_sim(location=location,
-                   calib_pars=calib_pars,
-                   analyzer=analyzer,
-                   vx_intv=[],
-                   hiv_datafile=hiv_datafile,
-                   art_datafile=art_datafile,
-                   end=2020,
-                   n_agents=50e3,
-                   hiv_death_adj=1
-                   )
+    sim = make_sim(
+        location=location,
+        calib_pars=calib_pars,
+        analyzers=analyzers,
+        vx_intv=[],
+        hiv_datafile=hiv_datafile,
+        art_datafile=art_datafile,
+        end=2020,
+        n_agents=50e3,
+        hiv_death_adj=1
+    )
 
     sim.run()
 
-    # New infections by age and sex
-    simres = sc.dcp(sim.results)
-    years = simres['year']
-    year_ind = sc.findinds(years, 1985)[0]
-    rsa_df = pd.read_csv('data/RSA_data.csv').set_index('Unnamed: 0').T
-    title_dict = dict(
-        female_hiv_prevalence='HIV prevalence (%), females 15+',
-        hiv_incidence='HIV incidence',
-        art_coverage='ART coverage (%)',
-    )
-    years = years[year_ind:]
-    fig, axes = pl.subplots(1, 3, figsize=(10, 4))
-    to_plot = ['female_hiv_prevalence', 'art_coverage', ['cancer_incidence_with_hiv', 'cancer_incidence_no_hiv']]
-    for iv, ax in enumerate(axes.flatten()):
-        val = to_plot[iv]
-        if isinstance(val, list):
-            for val_to_plot in val:
-                label = 'HIV+' if 'with_hiv' in val_to_plot else 'HIV-'
-                result = simres[val_to_plot][year_ind:]
-                result = np.convolve(list(result), np.ones(5), "valid") / 5
-                ax.plot(years[4:], result, label=label)
-            ax.legend()
-            ax.set_title('Cancer incidence (per 100k)')
-        else:
-
-            result = simres[val][year_ind:]
-            if iv == 0:
-                ax.plot(years, 100 * result, label='HPVsim')
-            else:
-                ax.plot(years, 100 * result)
-            thembisa_val_lb = f'{val}_lb'
-            thembisa_val_ub = f'{val}_ub'
-            if iv == 0:
-                ax.scatter(years, 100 * rsa_df[thembisa_val_lb][:-10], marker='_', label='Thembisa,\n95% uncertainty',
-                           color='grey')
-                ax.scatter(years, 100 * rsa_df[thembisa_val_ub][:-10], marker='_', color='grey')
-            else:
-                ax.scatter(years, 100 * rsa_df[thembisa_val_lb][:-10], marker='_', color='grey')
-                ax.scatter(years, 100 * rsa_df[thembisa_val_ub][:-10], marker='_', color='grey')
-            ax.set_title(title_dict[val])
-            if iv == 0:
-                ax.legend(title='Source')
-        ax.set_ylim(bottom=0)
-        sc.SIticks(ax)
-    fig.tight_layout()
-    fig_name = f'figures/hiv_fit_{location}.png'
-    sc.savefig(fig_name, dpi=100)
-
-    from scipy.stats import weibull_min
-    n_sample = 1000
-    fig, axes = pl.subplots(1,2, figsize=(8,8))
-    for i, adjust in enumerate([1, 2]):
-        ax=axes[i]
-        for age in [15, 20, 30, 40]:
-            shape = sim['hiv_pars']['time_to_hiv_death_shape']
-            scale = sim['hiv_pars']['time_to_hiv_death_scale'](age)
-            scale = np.maximum(scale, 0)
-            time_to_hiv_death = adjust * weibull_min.rvs(c=shape, scale=scale, size=n_sample)
-            ax.hist(time_to_hiv_death, alpha=0.4, label=f'age {age}')
-
-        title = ', 2x longer' if i == 1 else ''
-        ax.set_title(f'HIV progression (not on ART){title}')
-        ax.set_xlabel('Time to HIV mortality')
-    axes[0].legend(title='Age')
-    fig.tight_layout()
-    fig_name = f'figures/hiv_progression.png'
-    sc.savefig(fig_name, dpi=100)
+    # # New infections by age and sex
+    # simres = sc.dcp(sim.results)
+    # years = simres['year']
+    # year_ind = sc.findinds(years, 1985)[0]
+    # rsa_df = pd.read_csv('data/RSA_data.csv').set_index('Unnamed: 0').T
+    # title_dict = dict(
+    #     female_hiv_prevalence='HIV prevalence (%), females 15+',
+    #     hiv_incidence='HIV incidence',
+    #     art_coverage='ART coverage (%)',
+    # )
+    # years = years[year_ind:]
+    # fig, axes = pl.subplots(1, 3, figsize=(10, 4))
+    # to_plot = ['female_hiv_prevalence', 'art_coverage', ['cancer_incidence_with_hiv', 'cancer_incidence_no_hiv']]
+    # for iv, ax in enumerate(axes.flatten()):
+    #     val = to_plot[iv]
+    #     if isinstance(val, list):
+    #         for val_to_plot in val:
+    #             label = 'HIV+' if 'with_hiv' in val_to_plot else 'HIV-'
+    #             result = simres[val_to_plot][year_ind:]
+    #             result = np.convolve(list(result), np.ones(5), "valid") / 5
+    #             ax.plot(years[4:], result, label=label)
+    #         ax.legend()
+    #         ax.set_title('Cancer incidence (per 100k)')
+    #     else:
+    #
+    #         result = simres[val][year_ind:]
+    #         if iv == 0:
+    #             ax.plot(years, 100 * result, label='HPVsim')
+    #         else:
+    #             ax.plot(years, 100 * result)
+    #         thembisa_val_lb = f'{val}_lb'
+    #         thembisa_val_ub = f'{val}_ub'
+    #         if iv == 0:
+    #             ax.scatter(years, 100 * rsa_df[thembisa_val_lb][:-10], marker='_', label='Thembisa,\n95% uncertainty',
+    #                        color='grey')
+    #             ax.scatter(years, 100 * rsa_df[thembisa_val_ub][:-10], marker='_', color='grey')
+    #         else:
+    #             ax.scatter(years, 100 * rsa_df[thembisa_val_lb][:-10], marker='_', color='grey')
+    #             ax.scatter(years, 100 * rsa_df[thembisa_val_ub][:-10], marker='_', color='grey')
+    #         ax.set_title(title_dict[val])
+    #         if iv == 0:
+    #             ax.legend(title='Source')
+    #     ax.set_ylim(bottom=0)
+    #     sc.SIticks(ax)
+    # fig.tight_layout()
+    # fig_name = f'figures/hiv_fit_{location}.png'
+    # sc.savefig(fig_name, dpi=100)
+    #
+    # from scipy.stats import weibull_min
+    # n_sample = 1000
+    # fig, axes = pl.subplots(1,2, figsize=(8,8))
+    # for i, adjust in enumerate([1, 2]):
+    #     ax=axes[i]
+    #     for age in [15, 20, 30, 40]:
+    #         shape = sim['hiv_pars']['time_to_hiv_death_shape']
+    #         scale = sim['hiv_pars']['time_to_hiv_death_scale'](age)
+    #         scale = np.maximum(scale, 0)
+    #         time_to_hiv_death = adjust * weibull_min.rvs(c=shape, scale=scale, size=n_sample)
+    #         ax.hist(time_to_hiv_death, alpha=0.4, label=f'age {age}')
+    #
+    #     title = ', 2x longer' if i == 1 else ''
+    #     ax.set_title(f'HIV progression (not on ART){title}')
+    #     ax.set_xlabel('Time to HIV mortality')
+    # axes[0].legend(title='Age')
+    # fig.tight_layout()
+    # fig_name = f'figures/hiv_progression.png'
+    # sc.savefig(fig_name, dpi=100)
 
     # to_plot = {
     #     'HIV prevalence': [
